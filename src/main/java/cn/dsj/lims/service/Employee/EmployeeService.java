4c3a4e7bc59b4d78c67ab8694c14dd7ad2aa2d2c
zjh <2635414526@qq.com>
2019-02-26 10:44:56 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/cn/dsj/lims/controller/AssessController.java
    #	src/main/java/cn/dsj/lims/pojo/DetectionItem.java
    #	src/main/java/cn/dsj/lims/service/Assess/AssessServiceImpl.java
    #	src/main/java/cn/dsj/lims/util/DocConverter.java
    #	src/main/resources/application.properties
    #	src/main/resources/mapper/AssessMapper.xml
    #	src/main/resources/templates/assess.html

--- src/main/java/cn/dsj/lims/service/Employee/EmployeeService.java
+++ src/main/java/cn/dsj/lims/service/Employee/EmployeeService.java
@@ -6,5 +6,11 @@ import java.util.List;
 
 public interface EmployeeService {
     public List<Employee> getEmployeeList();
+    public List<Employee> list();
     public Employee getOneByUserId(int userId);
+
+    public boolean addEmployee(Employee employee);
+    public boolean updateEmployee(Employee employee);
+    public boolean deleteEmployee(int id);
+    public Employee getOneByStaffId(int staffId);
 }
--- src/main/java/cn/dsj/lims/service/Employee/EmployeeServiceImpl.java
+++ src/main/java/cn/dsj/lims/service/Employee/EmployeeServiceImpl.java
@@ -21,13 +21,46 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Resource
     private EmployeeMapper employeeMapper;
 
+
+
     @Override
     public List<Employee> getEmployeeList() {
         return employeeMapper.getEmployeeList();
     }
 
     @Override
+    public List<Employee> list() {
+        return employeeMapper.list();
+    }
+
+    @Override
     public Employee getOneByUserId(int userId) {
         return employeeMapper.getOneByUserId(userId);
+    }
+
+    @Override
+    public boolean addEmployee(Employee employee) {
+        if(employeeMapper.addEmployee(employee)>0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean updateEmployee(Employee employee) {
+        if(employeeMapper.updateEmployee(employee)>0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean deleteEmployee(int id) {
+        if (employeeMapper.deleteEmployee(id)>0)
+            return true;
+        return false;
+    }
+
+    @Override
+    public Employee getOneByStaffId(int staffId) {
+        return employeeMapper.getOneByStaffId(staffId);
     }
 }
