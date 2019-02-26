0bfb5bf57c1a0848faaa0883f4a5c2bec13475c8
zjh <2635414526@qq.com>
2019-02-26 10:44:11 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/cn/dsj/lims/controller/AssessController.java
    #	src/main/java/cn/dsj/lims/pojo/DetectionItem.java
    #	src/main/java/cn/dsj/lims/service/Assess/AssessServiceImpl.java
    #	src/main/java/cn/dsj/lims/util/DocConverter.java
    #	src/main/resources/application.properties
    #	src/main/resources/mapper/AssessMapper.xml
    #	src/main/resources/templates/assess.html

--- src/main/java/cn/dsj/lims/controller/EmployeeController.java
+++ src/main/java/cn/dsj/lims/controller/EmployeeController.java
@@ -2,8 +2,8 @@ package cn.dsj.lims.controller;
 
 import cn.dsj.lims.pojo.Employee;
 import cn.dsj.lims.service.Employee.EmployeeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,13 +16,39 @@ import java.util.List;
  * @create 2019/1/18
  */
 @RestController
-
+@RequestMapping("/employee")
 public class EmployeeController {
     @Resource
     private EmployeeService employeeService;
 
-    @RequestMapping("/employee")
-    public List<Employee> getEmployeeList(Employee employee) {
+    @RequestMapping("/list")
+    public List<Employee> getEmployeeList() {
         return employeeService.getEmployeeList();
+    }
+    @RequestMapping("/listDetails/{staffId}")
+    public Employee list(@PathVariable int staffId) {
+        return employeeService.getOneByStaffId(staffId);
+    }
+
+    @RequestMapping("/add")
+    public String addEmployee(Employee employee){
+        employee.setStaffId(Integer.parseInt(employee.getDeptName()));
+    if (employeeService.addEmployee(employee))
+        return "ture";
+    return "false";
+    }
+    @RequestMapping("/update")
+    public String updateEmployee(Employee employee){
+        employee.setStaffId(Integer.parseInt(employee.getDeptName()));
+        if (employeeService.updateEmployee(employee))
+            return "true";
+        return "false";
+    }
+    @RequestMapping("/delete")
+    public String deleteEmployee(@RequestParam("id") int staffId) {
+        System.out.println("delete...");
+        if (employeeService.deleteEmployee(staffId))
+            return "true";
+        return "false";
     }
 }
