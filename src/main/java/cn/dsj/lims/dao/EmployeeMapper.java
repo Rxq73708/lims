a089fcf04160f134ed2067dd628c59b8d67ba261
zjh <2635414526@qq.com>
2019-02-26 10:44:29 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/cn/dsj/lims/controller/AssessController.java
    #	src/main/java/cn/dsj/lims/pojo/DetectionItem.java
    #	src/main/java/cn/dsj/lims/service/Assess/AssessServiceImpl.java
    #	src/main/java/cn/dsj/lims/util/DocConverter.java
    #	src/main/resources/application.properties
    #	src/main/resources/mapper/AssessMapper.xml
    #	src/main/resources/templates/assess.html

--- src/main/java/cn/dsj/lims/dao/EmployeeMapper.java
+++ src/main/java/cn/dsj/lims/dao/EmployeeMapper.java
@@ -7,5 +7,15 @@ import java.util.List;
 public interface EmployeeMapper {
     public List<Employee> getEmployeeList();
 
+    public List<Employee> list();
+
     public Employee getOneByUserId(int userId);
+
+    public int addEmployee(Employee employee);
+
+    public int updateEmployee(Employee employee);
+
+    public int deleteEmployee(int staffId);
+
+    public Employee getOneByStaffId(int staffId);
 }
